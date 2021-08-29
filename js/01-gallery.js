59b0954061632d46aed7e5c73a40c3df317881c8
Artur Kraśniewski <artur.krasniewski1@gmail.com>
2021-08-29 18:43:08 +0200

    1 - prawie

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const qs = (selector) => document.querySelector(selector);
+const qsa = (selector) => document.querySelectorAll(selector);
+const log = (something) => console.log(something);
+const ce = (elem) => document.createElement(elem);
 
-console.log(galleryItems);
+const gallery = qs(".gallery");
+const createGalleryItems = (galleryItems) => {
+  for (const item of galleryItems) {
+    const galleryItem = ce("div");
+    galleryItem.classList.add("gallery__item");
+    gallery.append(galleryItem);
+
+    const galleryLink = ce("a");
+    galleryLink.classList.add("gallery__link");
+    galleryLink.href = item.original;
+    galleryItem.append(galleryLink);
+
+    const galleryImg = ce("img");
+    galleryImg.classList.add("gallery__image");
+    galleryImg.src = item.preview;
+    galleryImg.dataset.source = item.original;
+    galleryImg.alt = item.description;
+    galleryLink.append(galleryImg);
+  }
+};
+function selectImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  for (const item of galleryItems) {
+    if (event.target.src === item.preview) {
+      basicLightbox.create(`<img src=${item.original}>`).show();
+    }
+  }
+}
+
+createGalleryItems(galleryItems);
+gallery.addEventListener("click", selectImage);
